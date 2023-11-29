f3c354fba15a47a3fd768147802891f6c83c0b73
fritaim <114188034+fritaim@users.noreply.github.com>
2023-11-29 18:04:20 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+
+FPS = 60
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH // COLS
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Шашки')
+
+def main() -> None:
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+
+    pygame.quit()
+
+main()
