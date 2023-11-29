dbb32b5001f80899c857b16d0fa9a368af9af98e
fritaim <114188034+art3m4ik3@users.noreply.github.com>
2023-11-29 18:24:50 +0300

    Готова игра, но возможны баги

--- main.py
+++ main.py
@@ -4,12 +4,23 @@ from checkers.cfg import WIN, FPS, WIDTH, HEIGHT, SQUARE_SIZE, RED
 
 pygame.display.set_caption('Шашки')
 
+def get_row_col_from_mouse(pos) -> int:
+    x, y = pos
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
+    return row, col
+
 def main() -> None:
     run = True
     clock = pygame.time.Clock()
+    game = Game(WIN)
 
     while run:
         clock.tick(FPS)
+
+        if game.winner() != None:
+            print(game.winner())
+            run = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,6 +28,10 @@ def main() -> None:
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                game.select(row, col)
+
+        game.update()
 
     pygame.quit()
 
